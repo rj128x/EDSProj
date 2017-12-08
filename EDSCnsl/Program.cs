9256e83c9e2a8d42e182466449a6b161c3e505ea
rj128x <rj128x@gmail.com>
2017-12-08 09:21:50 +0500

    Добавьте файлы проекта.

---
+++ EDSCnsl/Program.cs
@@ -0,0 +1,22 @@
+﻿using EDSProj;
+using EDSProj.ModesCentre;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDSCnsl
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			Settings.init("Data/Settings.xml");
+			Logger.InitFileLogger(Settings.Single.LogPath, "pbrExport");
+			
+			MCSettings.init("Data/MCSettings.xml");
+
+			MCServerReader reader = new MCServerReader(DateTime.Now.Date);
+		}
+	}
+}
